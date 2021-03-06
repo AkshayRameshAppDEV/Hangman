0c3234a9c1746a0b179e24ed3764a9eba38237ab
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-06 15:22:43 -0600

    Added Initial iOS starter project

---
+++ Hangman/Hangman/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hangman/Hangman/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/6/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
