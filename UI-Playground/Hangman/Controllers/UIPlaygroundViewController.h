11a4d42e8d4c3805945dfb083ef68770071863c5
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-10 00:00:25 -0600

    Removed unecessary code in UI playground and renamed to UIPlaygriundviewcontroller

---
+++ UI-Playground/Hangman/Controllers/UIPlaygroundViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPlaygroundViewController : UIViewController
+
+@end
+
---
+++ UI-Playground/Hangman/Controllers/UIPlaygroundViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/6/21.
+//
+
+#import "UIPlaygroundViewController.h"
+
+@interface UIPlaygroundViewController ()
+
+@end
+
+@implementation UIPlaygroundViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
