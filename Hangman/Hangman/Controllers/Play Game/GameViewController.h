853f2c4554fa17958aefe912af7702a983360c38
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-10 23:30:44 -0600

    Added Game View controller for user to play hangman game

---
+++ Hangman/Hangman/Controllers/Play Game/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GameViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
