2a3c0248873224e9a7149bbaef7e0e3e0b7e2e26
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-13 09:27:09 -0600

    Show exit game alert when user presses back button

--- Hangman/Hangman/Controllers/Play Game/GameViewController.m
+++ Hangman/Hangman/Controllers/Play Game/GameViewController.m
@@ -15,19 +15,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self disableSideMenuGesture];
-    self.view.backgroundColor = UIColor.redColor;
+    self.navigationItem.hidesBackButton = YES;
+    UIBarButtonItem *newBackButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(exitGame:)];
+        self.navigationItem.leftBarButtonItem = newBackButton;
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-    NSLog(@"Back button called!");
-}
-
-- (void)disableSideMenuGesture {
-    if ([self.navigationController respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.navigationController.interactivePopGestureRecognizer.enabled = NO;
-    }
+- (void) exitGame:(UIBarButtonItem *)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"EXIT GAME" message:@"Do you want to exit game?" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"YES" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"NO" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:cancel];
+    [alert addAction:ok];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
