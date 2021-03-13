260861ba6d3a022f15b53c6c5f7cb152979ebc7f
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-13 10:59:47 -0600

    Localized game view strings and variable name updates

--- Hangman/Hangman/Controllers/Play Game/GameViewController.m
+++ Hangman/Hangman/Controllers/Play Game/GameViewController.m
@@ -16,19 +16,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.hidesBackButton = YES;
-    UIBarButtonItem *newBackButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(exitGame:)];
+    UIBarButtonItem *newBackButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"game_view_nav_left_bar_button_item_title", nil) style:UIBarButtonItemStylePlain target:self action:@selector(exitGame:)];
     self.navigationItem.leftBarButtonItem = newBackButton;
     self.view.backgroundColor = UIColor.whiteColor;
 }
 
 - (void) exitGame:(UIBarButtonItem *)sender {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"EXIT GAME" message:@"Do you want to exit game?" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"YES" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"game_view_exit_alert_title", nil) message:NSLocalizedString(@"game_view_exit_alert_message", nil) preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"game_view_exit_alert_yes", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         [self.navigationController popViewControllerAnimated:YES];
     }];
-    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"NO" style:UIAlertActionStyleCancel handler:nil];
-    [alert addAction:cancel];
-    [alert addAction:ok];
+    UIAlertAction *noAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"game_view_exit_alert_no", nil) style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:noAction];
+    [alert addAction:yesAction];
     [self presentViewController:alert animated:YES completion:nil];
 }
 
