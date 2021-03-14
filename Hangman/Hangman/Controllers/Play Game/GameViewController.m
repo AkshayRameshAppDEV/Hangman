cd2605af55d5898779f4350a558b59272124833b
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-13 18:44:02 -0600

    Updated visual structure of the alphabets arrray

--- Hangman/Hangman/Controllers/Play Game/GameViewController.m
+++ Hangman/Hangman/Controllers/Play Game/GameViewController.m
@@ -17,7 +17,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    alphabets = @[@[@"A", @"B", @"C", @"D", @"E"], @[@"F", @"G", @"H", @"I", @"J"], @[@"K", @"L", @"M", @"N", @"O"], @[@"P", @"Q", @"R", @"S", @"T"], @[@"U", @"V", @"W", @"X", @"Y"], @"Z"];
+    alphabets = @[@[@"A", @"B", @"C", @"D", @"E"],
+                  @[@"F", @"G", @"H", @"I", @"J"],
+                  @[@"K", @"L", @"M", @"N", @"O"],
+                  @[@"P", @"Q", @"R", @"S", @"T"],
+                  @[@"U", @"V", @"W", @"X", @"Y"],
+                  @"Z"
+                 ];
     [self setupNavbar];
     [self setupLabels];
     [self setupStackViews];
