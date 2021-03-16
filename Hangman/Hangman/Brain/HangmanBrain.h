b0146d8fdca5adee45f6ca67275d1245bd1f03a0
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-03-16 00:28:24 -0500

    Added initial hangman brain class

---
+++ Hangman/Hangman/Brain/HangmanBrain.h
@@ -0,0 +1,16 @@
+//
+//  HangmanBrain.h
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HangmanBrain : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hangman/Hangman/Brain/HangmanBrain.m
@@ -0,0 +1,12 @@
+//
+//  HangmanBrain.m
+//  Hangman
+//
+//  Created by Akshay Ramesh on 3/16/21.
+//
+
+#import "HangmanBrain.h"
+
+@implementation HangmanBrain
+
+@end
