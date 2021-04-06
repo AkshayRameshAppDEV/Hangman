6c45b20c3e1efad19f6818ad8c67862275558595
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-04-05 23:54:13 -0500

    Add dynamic params for word of the day and clue for word

--- Backend/ThemesServer/Sources/App/routes.swift
+++ Backend/ThemesServer/Sources/App/routes.swift
@@ -6,12 +6,12 @@ func routes(_ app: Application) throws {
         guard let tag = req.parameters.get("tag", as: Int.self) else {
             throw Abort(.badRequest)
         }
-        return try getThemeJSON(with: tag)
+        return try getThemeJSON(with: tag, wordOfTheDay: "Akshay", clueForWordOfTheDay: "name")
     }
 }
 
-func getThemeJSON(with tag: Int) throws -> String  {
-    let theme = Theme(wordOfTheDay: "Hello", clue: "phone")
+func getThemeJSON(with tag: Int, wordOfTheDay word: String, clueForWordOfTheDay clue: String) throws -> String  {
+    let theme = Theme(wordOfTheDay: word, clue: clue)
     let jsonEncoder = JSONEncoder()
     let jsonData = try jsonEncoder.encode(theme)
     guard let json = String(data: jsonData, encoding: String.Encoding.utf8) else {return "{}"}
