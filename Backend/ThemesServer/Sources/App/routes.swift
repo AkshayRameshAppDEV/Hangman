30d66470897769f07716c7639c4866c5ca30231c
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-04-05 23:38:28 -0500

    Added grouped theme route with tag

--- Backend/ThemesServer/Sources/App/routes.swift
+++ Backend/ThemesServer/Sources/App/routes.swift
@@ -1,15 +1,15 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
-
-    app.get("getTheme") { req -> String in
+    
+    app.get("themes", ":tag") { req -> String in
+        guard let tag = req.parameters.get("tag", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
         let theme = Theme(wordOfTheDay: "Hello", clue: "phone", type: "dictionary")
         let jsonEncoder = JSONEncoder()
         let jsonData = try jsonEncoder.encode(theme)
         guard let json = String(data: jsonData, encoding: String.Encoding.utf8) else {return "invalid"}
-        return json
+        return "\(json) + \(tag)"
     }
 }
