fb092413fbd313d2ec2635604630dc34df80d207
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-04-05 23:48:49 -0500

    Add separate function to handle theme json

--- Backend/ThemesServer/Sources/App/routes.swift
+++ Backend/ThemesServer/Sources/App/routes.swift
@@ -6,10 +6,14 @@ func routes(_ app: Application) throws {
         guard let tag = req.parameters.get("tag", as: Int.self) else {
             throw Abort(.badRequest)
         }
-        let theme = Theme(wordOfTheDay: "Hello", clue: "phone")
-        let jsonEncoder = JSONEncoder()
-        let jsonData = try jsonEncoder.encode(theme)
-        guard let json = String(data: jsonData, encoding: String.Encoding.utf8) else {return "invalid"}
-        return "\(json) + \(tag)"
+        return try getThemeJSON(with: tag)
     }
+}
+
+func getThemeJSON(with tag: Int) throws -> String  {
+    let theme = Theme(wordOfTheDay: "Hello", clue: "phone")
+    let jsonEncoder = JSONEncoder()
+    let jsonData = try jsonEncoder.encode(theme)
+    guard let json = String(data: jsonData, encoding: String.Encoding.utf8) else {return "{}"}
+    return "\(json) + \(tag)"
 }
