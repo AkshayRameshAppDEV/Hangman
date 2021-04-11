8e16ebe6b51df5f2efd4a904fbd44fa8ae248c8b
akshayrameshappdev <akshay_ramesh@yahoo.com>
2021-04-11 00:35:03 -0500

    Added Test for Hangman app

--- Frontend/Hangman/HangmanTests/HangmanTests.m
+++ Frontend/Hangman/HangmanTests/HangmanTests.m
@@ -6,6 +6,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "HangmanBrain.h"
+
 
 @interface HangmanTests : XCTestCase
 
@@ -13,24 +15,13 @@
 
 @implementation HangmanTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testHangmanBrain {
+    HangmanBrain *hangmanBrain = [[HangmanBrain alloc] init];
+    [hangmanBrain setWordOfTheDay:@"HANGMANTEST"];
+    XCTAssertEqual([hangmanBrain getWordOfTheDay], @"HANGMANTEST");
+    XCTAssertEqual([hangmanBrain getWordOfTheDayLength], 11);
+    XCTAssertEqual([[hangmanBrain getLetterLocations:@"Z"] count], 0);
+    XCTAssertNotNil([hangmanBrain getLetterLocations:@"H"]);
 }
 
 @end
